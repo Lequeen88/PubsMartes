3f302d6b8ac3ed334be618702090a89f17b3b6cb
Laura Isabel Garcia Ferrera <20221697@itla.edu.do>
2024-02-07 21:06:13 -0400

    Arquitectura Completada x2

---
+++ PubsMartes/PubsMartes.UnitTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace PubsMartes.UnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ PubsMartes/PubsMartes.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PubsMartes.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
